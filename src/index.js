c3f3e90842fdebecaef71cc1682a6e5a3268b660
Dean <dean.juricic@gmail.com>
2019-09-01 16:02:05 +0200

    extracted conditional logic from render method into a helper method

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Spinner from './Spinner'
 
 class App extends React.Component {
 
-	state = { lat: null, errorMessage: '' }; 
+	state = { lat: null, errorMessage: '' };
 	//This is kind of initialization is enabled by Babel, it creates constructor and initializes state for us
 
 	componentDidMount() {
@@ -20,18 +20,25 @@ class App extends React.Component {
 		console.log('Component updated!');
 	}
 
-	//React forces us to have method render
-	render() {
+	renderContent() {
 		if (this.state.errorMessage && !this.state.lat) {
 			return <div>Error: {this.state.errorMessage}</div >;
 		}
 		if (!this.state.errorMessage && this.state.lat) {
-			return <SeasonDisplay lat = {this.state.lat}/>;
+			return <SeasonDisplay lat={this.state.lat} />;
 		}
 
 		//If we didnt return from previous two conditions, it means we are still loading
-		return <Spinner text ="Please allow us to find your location"/>;
+		return <Spinner text="Please allow us to find your location" />;
+	}
 
+	//React forces us to have method render
+	render() {
+		return (
+			<div>
+				{this.renderContent()}
+			</div>
+		);
 	}
 }
 
