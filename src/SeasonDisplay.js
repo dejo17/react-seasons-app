3aa1394a6ff0c06943e57d5c81ea5eb28881f7ac
Dean <dean.juricic@gmail.com>
2019-09-01 11:59:48 +0200

    cleaned up repo fro default create-react-app files and added SeasonDisplay.js component file

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SeasonDisplay extends React.Component{
+	
+	render(){
+		
+		return <div>I am working OK! Latitude is: {props.lat}</div>
+		
+	}
+}
+export default SeasonDisplay;
